d5eec37a9e057a2346f3b324d3bf6192393c3993
Toh Jia Xuan <jiaxuan.toh10@rafflesgirlssch.edu.sg>
2023-07-22 11:02:09 +0800

    Initial Commit

---
+++ Singapore App/Singapore_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Singapore_AppApp.swift
+//  Singapore App
+//
+//  Created by RGS on 22/7/23.
+//
+
+import SwiftUI
+
+@main
+struct Singapore_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
